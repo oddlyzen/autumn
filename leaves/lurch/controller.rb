e753527b051ad752ac9841f0aa3a6757d66100a7
Mark Coates <mark.coates@gmail.com>
2009-03-23 11:59:13 -0400

    Cleaning up project--removing the Scorekeeper leaf. Using the new 'SunDial' Season. Need to pull Noah's changes from his fork that includes the #!wtf command. On branch master Changes to be committed:   (use "git reset HEAD <file>..." to unstage)
      modified:   config/global.yml
      deleted:    config/seasons/sun_dial/database.yml
      modified:   config/seasons/sun_dial/leaves.yml
      modified:   config/seasons/sun_dial/stems.yml
      modified:   config/seasons/testing/leaves.yml
      modified:   config/seasons/testing/stems.yml
      modified:   leaves/lurch/controller.rb
      modified:   leaves/lurch/views/end.txt.erb
      deleted:    leaves/scorekeeper/README
      deleted:    leaves/scorekeeper/config.yml
      deleted:    leaves/scorekeeper/controller.rb
      deleted:    leaves/scorekeeper/data/.gitignore
      deleted:    leaves/scorekeeper/helpers/general.rb
      deleted:    leaves/scorekeeper/models/channel.rb
      deleted:    leaves/scorekeeper/models/person.rb
      deleted:    leaves/scorekeeper/models/pseudonym.rb
      deleted:    leaves/scorekeeper/models/score.rb
      deleted:    leaves/scorekeeper/tasks/stats.rake
      deleted:    leaves/scorekeeper/views/about.txt.erb
      deleted:    leaves/scorekeeper/views/change.txt.erb
      deleted:    leaves/scorekeeper/views/history.txt.erb
      deleted:    leaves/scorekeeper/views/points.txt.erb
      deleted:    leaves/scorekeeper/views/usage.txt.erb
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
      leaves/lurch/config.yml
      leaves/lurch/views/tag.txt.erb

--- leaves/lurch/controller.rb
+++ leaves/lurch/controller.rb
@@ -62,14 +62,28 @@ class Controller < Autumn::Leaf
       @@TASKS.delete t
     end
     var :records => records
-    synchronize_with_server(records)
+    begin
+      synchronize_with_server(records)
+    rescue
+      var :exception => "An error occurred."
+    end
   end
   
   def gist_command(stem, sender, reply_to, msg)
     # hmmm...
   end
   
-  
+  def tag_command(stem, sender, reply_to, msg)
+    tags = msg.split(',')
+    @@TASKS.each do |t|
+      if t.user == sender
+        tags.each do |tag|
+          t.tag! tag
+        end
+      end
+    end
+    var :tags => tags.join(',')
+  end
   
   def about_command(stem, sender, reply_to, msg)
     'Lurch is a time-keeping bot (Autumn Leaf) developed by Mark Coates & Noah Sussman at ZepFrog Corp. http://zepfrog.com/development'
@@ -82,8 +96,12 @@ class Controller < Autumn::Leaf
   end
   def synchronize_with_server(records)
     init_slim_timer
+    task = nil
     records.each do |r|
-      @timer.create_timeentry r.started_at, r.time_elapsed, r.id, r.ended_at, r.tags
+      task = @timer.create_task r.description, t.tags
+      sleep(1)
+      @timer.create_timeentry r.started_at, r.time_elapsed, task['id'], r.ended_at, r.tags
+      sleep(1)
     end
   end
   
@@ -94,24 +112,33 @@ module Timing
   class Task < Object
     attr_accessor :user, :description, :total_time, :ended_at, :started_at, :tags
     def initialize(args={})
-      @started_at = Time.now
+      @started_at = Time.new
       @user = args[:user]
       @description = args[:description]
       @tags = args[:tags]
       @coworker_emails = args[:coworker_emails] || nil
       @reporter_emails = args[:reporter_emails] || nil
       @ended_at = nil
-      @total_time = 0.0
+      @total_time = 0
       @tags = []
     end
     
     def end!(reason)
-      @ended_at = Time.now
-      @total_time = time_elapsed + (@total_time || 0)
+      @ended_at = Time.new
+      if @total_time
+        @total_time += time_elapsed
+      end
+      @total_time ||= time_elapsed
     end
+    
+    def tag!(tag)
+      @tags << tag
+    end
+    
     def current_time
-      Time.now
+      Time.new
     end
+    
     def time_elapsed
       if @ended_at.nil?
         current_time - @started_at 
@@ -119,26 +146,33 @@ module Timing
         @ended_at - @started_at
       end
     end
+    
     def tags
       @tags || nil
     end
+    
     def tags=(array)
       @tags = array
     end
+    
     def coworker_emails
       @coworker_emails || []
     end
+    
     def reporter_emails
       @reporter_emails || []
     end
+    
     def coworker_emails=(array)
       @coworker_emails = array
     end
+    
     def reporter_emails=(array)
       @reporter_emails = array
     end
+    
     def resume!(msg)
-      @started_at = Time.now
+      @started_at = Time.new
       @ended_at = nil
     end
   end
